c5691a9fb1e32fe4c2b55656047cdfeeda7fe36f
nlare at PIRS <vereskanthem@gmail.com>
2016-11-28 18:09:49 +0600

    From JSON from JS

---
+++ src/GetJSONFromJS.java
@@ -0,0 +1,21 @@
+/**
+ * Created by nlare on 28.11.16.
+ */
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import javax.servlet.http.HttpServlet;
+
+public class GetJSONFromJS extends HttpServlet {
+
+//    protected String jsonString;
+//    protected JSONObject jsonObject;
+//    int getJSON()   {
+//
+//        jsonString = request.getParameter("jsonData");
+//        jsonObject = (JSONObject) JSONValue.parse(jsonString);
+//
+//    }
+
+}
